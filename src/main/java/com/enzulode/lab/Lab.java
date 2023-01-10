070f5701e5f779bf8e342f97d8945fa59d4abba6
Vladislav <andreevvlad443@gmail.com>
2023-01-10 03:07:47 +0300

    project structure upd: main logic moved out from subproject

--- src/main/java/com/enzulode/lab/Lab.java
+++ src/main/java/com/enzulode/lab/Lab.java
@@ -16,6 +16,7 @@ import com.enzulode.lab.interactions.Interaction;
 import com.enzulode.lab.util.ConsolePrinter;
 import com.enzulode.lab.util.Printer;
 import com.enzulode.processor.MarkedField;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
