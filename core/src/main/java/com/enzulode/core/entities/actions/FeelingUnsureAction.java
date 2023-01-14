18e300de2086915b019859cfc1d06ed420591fb9
Vladislav <andreevvlad443@gmail.com>
2023-01-15 00:56:56 +0300

    lab upd: feeling action impelmented

---
+++ core/src/main/java/com/enzulode/core/entities/actions/FeelingUnsureAction.java
@@ -0,0 +1,12 @@
+package com.enzulode.core.entities.actions;
+
+public class FeelingUnsureAction implements EntityAction
+{
+
+	@Override
+	public String perform()
+	{
+		return "почувствовал себя неуверенно";
+	}
+
+}
