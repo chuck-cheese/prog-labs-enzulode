0f80c2c60e0e38a3fba41b8bcdf26e323f534144
Vladislav <andreevvlad443@gmail.com>
2023-01-19 05:41:03 +0300

    lab upd: temporary entry point implemented

--- src/main/java/com/enzulode/lab/Lab.java
+++ src/main/java/com/enzulode/lab/Lab.java
@@ -1,8 +1,36 @@
 package com.enzulode.lab;
 
+import com.enzulode.core.TicketFactory;
+import com.enzulode.core.repository.Ticket;
+import java.lang.reflect.InvocationTargetException;
+
 public class Lab
 {
 	public static void main(String[] args)
 	{
+
+		try
+		{
+			Ticket ticket = (new TicketFactory()).newTicket(
+					"Dude",
+					172.123F,
+					283.22F,
+					999,
+					17.5F,
+					Ticket.TicketType.VIP,
+					new Ticket.Person(
+							"GIGA",
+							Ticket.EyeColor.BLUE,
+							Ticket.HairColor.WHITE,
+							Ticket.Country.USA
+					)
+			);
+
+			System.out.println(ticket);
+		}
+		catch (InstantiationException | IllegalAccessException | InvocationTargetException e)
+		{
+			System.out.println(e.getMessage());
+		}
 	}
 }
