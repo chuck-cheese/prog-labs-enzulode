fa334a5c79d2783e3935215e9c7a5b2931d20077
Vladislav <andreevvlad443@gmail.com>
2023-01-29 17:18:27 +0300

    ticket factory is depreated: non-class-dependant instance factory implemented

--- core/src/main/java/com/enzulode/core/InstanceFactory.java
+++ core/src/main/java/com/enzulode/core/InstanceFactory.java
@@ -5,7 +5,6 @@ import com.enzulode.core.exceptions.FieldCannotBeNullException;
 import com.enzulode.core.exceptions.FloatIsOutOfBorderException;
 import com.enzulode.core.exceptions.IntegerIsOutOfBorderException;
 import com.enzulode.core.exceptions.StringCannotBeEmptyException;
-import com.enzulode.core.repository.Ticket;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -13,42 +12,38 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Random;
 
-public class TicketFactory
+public class InstanceFactory
 {
 
-	public TicketFactory()
+	public static Object instantiate(Class clazz, Object[] args)
+			throws InvocationTargetException, InstantiationException, IllegalAccessException
 	{
-	}
 
-	public Ticket newTicket(String name, float coordX, float coordY, Integer price, float discount, Ticket.TicketType type, Ticket.Person person)
-			throws InstantiationException, IllegalAccessException, InvocationTargetException
-	{
-		Class<Ticket> ticketClass = Ticket.class;
-		Constructor<?> ticketConstructor = ticketClass.getDeclaredConstructors()[0];
-		ticketConstructor.setAccessible(true);
-		Ticket ticket = (Ticket) ticketConstructor.newInstance(name, coordX, coordY, price, discount, type, person);
-		Field[] ticketFields = ticketClass.getDeclaredFields();
+		Constructor<?> clazzConstructor = clazz.getDeclaredConstructors()[0];
+		clazzConstructor.setAccessible(true);
+		Object obj =  clazzConstructor.newInstance(args);
+		Field[] clazzFields = clazz.getDeclaredFields();
 
-		for (Field field : ticketFields)
+		for (Field field : clazzFields)
 		{
 			field.setAccessible(true);
 
 			if (field.isAnnotationPresent(AutoID.class))
-				field.set(ticket, (new Random()).nextLong(1, Long.MAX_VALUE));
+				field.set(obj, (new Random()).nextLong(1, Long.MAX_VALUE));
 
 			if (field.isAnnotationPresent(AutoDate.class))
-				field.set(ticket, ZonedDateTime.now(ZoneId.systemDefault()));
+				field.set(obj, ZonedDateTime.now(ZoneId.systemDefault()));
 
-			if (field.isAnnotationPresent(NotEmptyString.class) && "".equals(field.get(ticket)))
+			if (field.isAnnotationPresent(NotEmptyString.class) && "".equals(field.get(obj)))
 				throw new StringCannotBeEmptyException(field.getName());
 
-			if (field.isAnnotationPresent(NotNull.class) && field.get(ticket) == null)
+			if (field.isAnnotationPresent(NotNull.class) && field.get(obj) == null)
 				throw new FieldCannotBeNullException(field.getName());
 
 			if (field.isAnnotationPresent(BorderedInteger.class))
 			{
 				BorderedInteger borderedInteger = field.getAnnotation(BorderedInteger.class);
-				int value = (int) field.get(ticket);
+				int value = (int) field.get(obj);
 
 				if (value > borderedInteger.max() || value < borderedInteger.min())
 					throw new IntegerIsOutOfBorderException(field.getName());
@@ -57,7 +52,7 @@ public class TicketFactory
 			if (field.isAnnotationPresent(BorderedFloat.class))
 			{
 				BorderedFloat borderedFloat = field.getAnnotation(BorderedFloat.class);
-				float value = (float) field.get(ticket);
+				float value = (float) field.get(obj);
 
 				if (value > borderedFloat.max() || value < borderedFloat.min())
 					throw new FloatIsOutOfBorderException(field.getName());
@@ -65,7 +60,8 @@ public class TicketFactory
 
 		}
 
-		return ticket;
+		return obj;
+
 	}
 
 }
