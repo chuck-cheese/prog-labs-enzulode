45852564a750dc9ddd695e096c9b7d7fde06004e
Vladislav <andreevvlad443@gmail.com>
2023-01-29 17:27:15 +0300

    application command not found exception implemented

---
+++ core/src/main/java/com/enzulode/core/exceptions/CommandNotFoundException.java
@@ -0,0 +1,11 @@
+package com.enzulode.core.exceptions;
+
+public class CommandNotFoundException extends Exception
+{
+
+	public CommandNotFoundException(String description)
+	{
+		super(description);
+	}
+
+}
