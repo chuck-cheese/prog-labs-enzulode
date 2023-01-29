6821f64f4466c772db1a4c08c8bfebad5ff44270
Vladislav <andreevvlad443@gmail.com>
2023-01-29 17:28:28 +0300

    lab main class now uses application main class

--- src/main/java/com/enzulode/lab/Lab.java
+++ src/main/java/com/enzulode/lab/Lab.java
@@ -1,36 +1,12 @@
 package com.enzulode.lab;
 
-import com.enzulode.core.TicketFactory;
-import com.enzulode.core.repository.Ticket;
-import java.lang.reflect.InvocationTargetException;
+import com.enzulode.core.App;
 
 public class Lab
 {
 	public static void main(String[] args)
 	{
-
-		try
-		{
-			Ticket ticket = (new TicketFactory()).newTicket(
-					"Dude",
-					172.123F,
-					283.22F,
-					999,
-					17.5F,
-					Ticket.TicketType.VIP,
-					new Ticket.Person(
-							"GIGA",
-							Ticket.EyeColor.BLUE,
-							Ticket.HairColor.WHITE,
-							Ticket.Country.USA
-					)
-			);
-
-			System.out.println(ticket);
-		}
-		catch (InstantiationException | IllegalAccessException | InvocationTargetException e)
-		{
-			System.out.println(e.getMessage());
-		}
+		App app = new App();
+		app.run();
 	}
 }
